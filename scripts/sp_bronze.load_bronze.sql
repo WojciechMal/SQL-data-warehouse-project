03cc6a6ab93f4d7c44a273ffa32dfe1eee5ce2bf
WojciechMal <101119195+WojciechMal@users.noreply.github.com>
2025-05-22 09:31:07 +0200

    Update sp_bronze.load_bronze.sql

--- scripts/sp_bronze.load_bronze.sql
+++ scripts/sp_bronze.load_bronze.sql
@@ -1,7 +1,18 @@
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
+	PRINT '===============================';
+	PRINT 'Load Bronze Layer';
+	PRINT '===============================';
+	
+	PRINT '-------------------------------';
+	PRINT 'Loading CRM Tables';
+	PRINT '-------------------------------';
+
+	PRINT '>> Truncating Table: [bronze].[crm_cust_info]'
+
 	TRUNCATE TABLE [bronze].[crm_cust_info]
 
+	PRINT '>> Inserting Data Into: [bronze].[crm_cust_info]'
 	BULK INSERT [bronze].[crm_cust_info]
 	FROM 'C:\Users\wojciech.malinowski_\Desktop\Projects\dwh\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 	WITH (
@@ -10,8 +21,9 @@ BEGIN
 		TABLOCK
 	);
 
+	PRINT '>> Truncating Table: [bronze].[crm_prd_info]'
 	TRUNCATE TABLE [bronze].[crm_prd_info]
-
+	PRINT '>> Inserting Data Into: [bronze].[crm_prd_info]'
 	BULK INSERT [bronze].[crm_prd_info]
 	FROM 'C:\Users\wojciech.malinowski_\Desktop\Projects\dwh\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 	WITH (
@@ -20,8 +32,9 @@ BEGIN
 		TABLOCK
 	);
 
+	PRINT '>> Truncating Table: [bronze].[crm_sales_details]'
 	TRUNCATE TABLE [bronze].[crm_sales_details]
-
+	PRINT '>> Inserting Data Into: [bronze].[crm_sales_details]'
 	BULK INSERT [bronze].[crm_sales_details]
 	FROM 'C:\Users\wojciech.malinowski_\Desktop\Projects\dwh\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 	WITH (
@@ -30,8 +43,13 @@ BEGIN
 		TABLOCK
 	);
 
-	TRUNCATE TABLE [bronze].[erp_cust_az12]
+	PRINT '-------------------------------';
+	PRINT 'Loading ERP Tables';
+	PRINT '-------------------------------';
 
+	PRINT '>> Truncating Table: [bronze].[erp_cust_az12]'
+	TRUNCATE TABLE [bronze].[erp_cust_az12]
+	PRINT '>> Inserting Data Into: [bronze].[erp_cust_az12]'
 	BULK INSERT [bronze].[erp_cust_az12]
 	FROM 'C:\Users\wojciech.malinowski_\Desktop\Projects\dwh\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
 	WITH (
@@ -40,8 +58,9 @@ BEGIN
 		TABLOCK
 	);
 
+	PRINT '>> Truncating Table: [bronze].[erp_loc_a101]'
 	TRUNCATE TABLE [bronze].[erp_loc_a101]
-
+	PRINT '>> Inserting Data Into: [bronze].[erp_loc_a101]'
 	BULK INSERT [bronze].[erp_loc_a101]
 	FROM 'C:\Users\wojciech.malinowski_\Desktop\Projects\dwh\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
 	WITH (
@@ -50,8 +69,9 @@ BEGIN
 		TABLOCK
 	);
 
+	PRINT '>> Truncating Table: [bronze].[erp_px_cat_g1v2]'
 	TRUNCATE TABLE [bronze].[erp_px_cat_g1v2]
-
+	PRINT '>> Inserting Data Into: [bronze].[erp_px_cat_g1v2]'
 	BULK INSERT [bronze].[erp_px_cat_g1v2]
 	FROM 'C:\Users\wojciech.malinowski_\Desktop\Projects\dwh\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
 	WITH (
